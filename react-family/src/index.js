0f62b78fbf0715b6fdbbcd650853fea80deed506
wwjhzc <2258660979@qq.com>
2018-05-10 22:47:23 +0800

    提交

---
+++ react-family/src/index.js
@@ -0,0 +1,4 @@
+const func = str => {
+    document.getElementById('app').innerHTML = str;
+  };
+  func('我现在在使用Babel!');
---
+++ react-family/webpack.dev.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+module.exports = {
+ //入口文件，刚才初始化项目填写信息时有写，因人而异
+ entry:path.join(__dirname, '/src/index.js'),
+ //输出到dist文件夹，输出的名字叫bundle.js
+ output:{
+     path:path.join(__dirname, './dist'),
+     filename:'bundle.js'
+ },
+ module: {
+     rules: [{
+         test: /\.js$/,
+         use: ['babel-loader?cacheDirectory=true'],
+         include: path.join(__dirname, 'src')
+     }]
+ }
+}
+
